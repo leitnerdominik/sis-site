bfe607aef5f44b9534388e80e2eeda973d28bebe
Dominik Leitner <bigtoasti@gmail.com>
2018-12-31 16:57:03 +0100

    google fonts..

---
+++ src/components/SEO/SEO.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const seo = () => (
+  <Helmet>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" /> 
+  </Helmet>
+);
+
+export default seo;
