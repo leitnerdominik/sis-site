e257370a5d85d33d86ac01d4d0dba3fc71ff0e5a
Dominik Leitner <bigtoasti@gmail.com>
2018-12-31 16:57:57 +0100

    getting images with graphql

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,28 +2,46 @@ import React, { Fragment } from 'react';
 import Slider from 'react-slick';
 import '../../../node_modules/slick-carousel/slick/slick.css';
 import '../../../node_modules/slick-carousel/slick/slick-theme.css';
+import Img from 'gatsby-image';
 
 import classes from './Hero.module.css';
 
-const hero = () => {
+const hero = ({ images }) => {
   const settings = {
-    dots: true,
-    infinite: true,
     autoplay: true,
     autoplaySpeed: 10000,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
   };
+
+  const imgs = images.map(img => (
+    <div key={img.node.id}>
+      <div className={classes.Text}>
+        <p>Manchmal, da möchte ich mich zurückziehen können</p>
+        <p>Tief in mein Haus. Ruhe.</p>
+        <p>Nichts dringt hinein. Nichts hinaus.</p>
+        <p>Geborgen.</p>
+      </div>
+      <div className={classes.Images}>
+        <Img className={classes.Image} fluid={img.node.childImageSharp.fluid} />
+      </div>
+    </div>
+  ));
 
   return (
     <Fragment>
       <Slider {...settings}>
         <header className={`${classes.HeroContainer} ${classes.Image1}`}>
-          <section className={classes.Content}>test1</section>
+          <section className={classes.Text}>
+            <p>Manchmal, da würde ich meine Hülle gern loswerden. Frei.</p>
+            <p>Etwas wagen. Kontakt haben.</p>
+            <p>Vielleicht gar Zuwendung erfahren.</p>
+            <p>Risiko.</p>
+          </section>
         </header>
         <header className={`${classes.HeroContainer} ${classes.Image2}`}>
-          <section className={classes.Content}>test2</section>
+          <section className={classes.Text}><p>Manchmal, da möchte ich mich zurückziehen können.</p>
+            <p>Tief in mein Haus. Ruhe.</p>
+            <p>Nichts dringt hinein. Nichts hinaus.</p>
+            <p>Geborgen.</p></section>
         </header>
       </Slider>
     </Fragment>
