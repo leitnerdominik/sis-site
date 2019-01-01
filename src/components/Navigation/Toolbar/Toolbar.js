1653b9719340a2ec3e1ddb60a948ed9a96f00c22
Dominik <leitner.dominik@hotmail.de>
2019-01-01 21:49:55 +0100

    added mobile support and transparent to solid navbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,40 @@
 import React from 'react';
 
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../DrawerToggle/DrawerToggle';
+import Logo from '../../Logo/Logo';
 
 import classes from './Toolbar.module.css';
 
-const toolbar = ({page1, page2, page3}) => (
-  <div className={classes.Container}>
-    <nav>
-      <NavigationItems pages={[page1, page2, page3]}/>
-    </nav>
-  </div>
-);
+const toolbar = props => {
+  const {
+    page0,
+    page1,
+    page2,
+    page3,
+    isNavTransparent,
+    drawerClicked,
+    sideDrawerOpen,
+  } = props;
+
+  const navBarClasses = [classes.Container];
+  if (!isNavTransparent) {
+    navBarClasses.push(classes.Solid);
+  }
+
+  return (
+    <div className={navBarClasses.join(' ')}>
+      <DrawerToggle clicked={drawerClicked} sideDrawerOpen={sideDrawerOpen} />
+      <div className={classes.Toolbar}>
+        <div className={classes.DesktopOnly}>
+          <Logo clicked={page0} isSolid={!isNavTransparent}/>
+        </div>
+        <nav className={classes.NavItems}>
+          <NavigationItems pages={[page1, page2, page3]} />
+        </nav>
+      </div>
+    </div>
+  );
+};
 
 export default toolbar;
