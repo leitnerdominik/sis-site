a1057e0d43e87293c65fd20eef686ab2cfc36656
Dominik <leitner.dominik@hotmail.de>
2019-01-06 11:25:04 +0100

    adjusted text on mobile devices

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react';
 import Slider from 'react-slick';
 import '../../../node_modules/slick-carousel/slick/slick.css';
 import '../../../node_modules/slick-carousel/slick/slick-theme.css';
-import Img from 'gatsby-image';
 
 import classes from './Hero.module.css';
 
